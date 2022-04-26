c47ee5656a27ace68b10d57f3fd897dc7fe723e2
Hau Dao <hau.dao@employmenthero.com>
2022-04-26 14:30:23 +0700

    Use elsint instead of elsint_d

--- nvim/after/plugin/lspconfig.rc.lua
+++ nvim/after/plugin/lspconfig.rc.lua
@@ -65,9 +65,9 @@ null_ls.setup({
   on_attach = on_attach,
   root_dir = u.root_pattern("package.json"),
   sources = {
-    null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.code_actions.eslint,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.formatting.eslint,
     null_ls.builtins.formatting.prettierd,
   }
 })
