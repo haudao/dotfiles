2f465b15d441a0ee2cf3a582e7cf8faf84141f23
Hau Dao <hau.dao@employmenthero.com>
2022-06-28 21:39:08 +0700

    Fix not formating code

--- nvim/after/plugin/lspconfig.rc.lua
+++ nvim/after/plugin/lspconfig.rc.lua
@@ -53,11 +53,7 @@ end
 
 -- TypeScript
 nvim_lsp.tsserver.setup {
-  on_attach = function(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
-    on_attach(client, bufnr)
-  end,
+  on_attach = on_attach,
   filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
 }
 
